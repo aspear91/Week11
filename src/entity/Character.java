dd8cb11d9d30f44ef96c640dd79e8220c32eefcc
Andy <andy9pearson1@gmail.com>
2019-04-11 18:18:41 -0700

    first commit

--- src/entity/Character.java
+++ src/entity/Character.java
@@ -2,23 +2,13 @@ package entity;
 
 public class Character {
 	
-//	private int characterId;
 	private String firstName;
 	private String lastName;
 	
 	public Character(String firstName, String lastName){
-//		this.setCharacterId(characterId);
 		this.setFirstName(firstName);
 		this.setLastName(lastName);
 	}
-
-/*	public int getCharacterId() {
-		return characterId;
-	}
-
-	public void setCharacterId(int characterId) {
-		this.characterId = characterId;
-	}*/
 
 	public String getFirstName() {
 		return firstName;
